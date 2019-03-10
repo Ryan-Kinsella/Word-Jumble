598352d3d555cb1cf4e076ad069a0b01bfbb65a9
Ryan Kinsella <15rklk@queensu.ca>
2019-03-10 19:52:38 -0400

    jumble created and commit

---
+++ jumble.h
@@ -0,0 +1,57 @@
+/*
+ * jumble.h
+ *
+ *  Created on: Nov 16, 2018
+ *      Author: Ryan Kinsella
+ */
+//Ryan Kinsella, 10194574, 15rklk
+
+#pragma once
+#include <string>
+using namespace std;
+
+typedef char* charArrayPtr; //told to implement this in assignment handout
+
+class BadJumbleException {
+public:
+	BadJumbleException(const string&);
+	string what() const;
+private:
+	string message;
+}; //end of exception class
+
+class JumblePuzzle {
+public:
+	JumblePuzzle(); //default constructor
+	JumblePuzzle(string hiddenWord,string difficulty); //two parameter constructor
+	JumblePuzzle(const JumblePuzzle&); //copy constructor
+	~JumblePuzzle(); //destructor
+	JumblePuzzle& operator=(const JumblePuzzle& jp); //overloading assignment operator
+
+
+	//accessors
+	int getRowPos();
+	int getColPos();
+	int getSize();
+	char getDirection();
+	string getWord();
+	string getDifficulty();
+	charArrayPtr* getJumble(); //this is the double pointer to heap
+
+private:
+	//insert words for each direction
+	bool hidden=false;
+	bool insertNorth(int, int);
+	bool insertSouth(int, int);
+	bool insertEast(int, int);
+	bool insertWest(int, int);
+	void insertWord();
+
+	string word;
+	string difficulty;
+	int rowPos;
+	int colPos;
+	int size;
+	char direction;
+	charArrayPtr* jumble;
+};
